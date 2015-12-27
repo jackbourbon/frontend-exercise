db565d554832cd87dc4d461561db86bb7e48186c
Alberto Vila <alberto.vilatena@gmail.com>
2015-12-27 19:35:53 +0200

    Very basic view of the drivers in the index

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,9 @@
+'use strict'
 
+var grandprixApp = angular.module('grandprixApp', []);
+
+grandprixApp.controller('StandingsCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('api/standings.json').success(function(data){
+    $scope.drivers = data
+  });
+}]);
