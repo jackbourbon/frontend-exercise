b808beb94656560d621391d167ef91a2c3848212
Alberto Vila <alberto.vilatena@gmail.com>
2015-12-27 19:33:56 +0200

    Changes for the backend

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-import os.path
-from flask import Flask
+import os.path, json
+from flask import Flask, session
+from IPython import embed
+from random import randint
 
 SRC_DIR = os.path.abspath(os.path.dirname(__file__))
 DATA_DIR = os.path.join(SRC_DIR, 'data')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'top-secret!'
+
+def get_drivers():
+    if not session.get('drivers'):
+        with open(DATA_DIR + '/drivers.json') as data_file:
+            session['drivers'] = json.load(data_file)
+    return session['drivers']
+
+def increment_points(driver_list):
+    driver_index = (randint(0,len(driver_list)))
+    driver_list[driver_index]['points'] = driver_list[driver_index]['points'] + 1
+    return driver_list
+
+def get_teams_by_id():
+    if not session.get('teams_by_id'):
+        with open(DATA_DIR + '/teams.json') as data_file:
+            data = json.load(data_file)
+            session['teams_by_id'] = dict(map(lambda x: (x['id'], x), data))
+    return session['teams_by_id']
 
 @app.route('/')
 def home():
@@ -12,14 +33,14 @@ def home():
 
 @app.route('/api/standings.json')
 def standings():
-    # TODO: Implement
-    pass
+    drivers = increment_points(get_drivers())
+    return json.dumps(drivers)
 
 
-@app.route('/pai/team/<int:team_id>.json')
+@app.route('/api/team/<int:team_id>.json')
 def team_details(team_id):
-    # TODO: Implement
-    pass
+    teams_by_id = get_teams_by_id()
+    return json.dumps(teams_by_id[str(team_id)])
 
 if __name__ == '__main__':
     app.run(debug=True)
