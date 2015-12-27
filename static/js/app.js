2326dcf85bf99af5d5f25b489bc36dd82b1041bf
Alberto Vila <alberto.vilatena@gmail.com>
2015-12-28 00:34:54 +0200

    Adding routes, basic team page and interval calls in standings

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,34 @@
-'use strict'
+var grandprixApp = angular.module('grandprixApp', ['ngRoute']);
 
-var grandprixApp = angular.module('grandprixApp', []);
+grandprixApp.config(['$routeProvider', function($routeProvider){
+    $routeProvider.
+      when('/', {
+        templateUrl: 'static/partials/standings.html',
+        controller: 'StandingsCtrl'
+      }).
+      when('/teams/:teamId', {
+        templateUrl: 'static/partials/team-detail.html',
+        controller: 'TeamDetailCtrl'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+}]);
 
-grandprixApp.controller('StandingsCtrl', ['$scope', '$http', function($scope, $http) {
+grandprixApp.controller('StandingsCtrl', ['$scope', '$http', '$interval', function($scope, $http, $interval){
+  $interval(function() {
+    $http.get('api/standings.json').success(function(data){
+      $scope.drivers = data;
+    });
+  }, 1000);
+}]);
+
+grandprixApp.controller('TeamDetailCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
+  $http.get('api/team/' + $routeParams.teamId + '.json').success(function(data){
+    $scope.team = data;
+  });
+
   $http.get('api/standings.json').success(function(data){
-    $scope.drivers = data
+    $scope.drivers = data;
   });
 }]);
