43cbe62fbe57989e0ca6d19864a71fc34eda1d70
Alberto Vila <alberto.vilatena@gmail.com>
2015-12-30 19:31:08 +0200

    Simplifying the backend

--- app.py
+++ app.py
@@ -11,23 +11,20 @@ app.config.from_object(__name__)
 
 app.config['SECRET_KEY'] = 'top-secret!'
 
-def get_drivers():
-    if not session.get('drivers'):
-        with open(DATA_DIR + '/drivers.json') as data_file:
-            session['drivers'] = json.load(data_file)
-    return session['drivers']
+def get_data_from_file(filename):
+    data = None
+    with open(DATA_DIR + '/' + filename) as data_file:
+        data = json.load(data_file)
+    return data
 
 def increment_points(driver_list):
     driver_index = (randint(0,len(driver_list)-1))
     driver_list[driver_index]['points'] = driver_list[driver_index]['points'] + 1
     return driver_list
 
-def get_teams_by_id():
-    if not session.get('teams_by_id'):
-        with open(DATA_DIR + '/teams.json') as data_file:
-            data = json.load(data_file)
-            session['teams_by_id'] = dict(map(lambda x: (str(x['id']), x), data))
-    return session['teams_by_id']
+def update_drivers(drivers_data):
+    with open(DATA_DIR + '/drivers.json', 'w') as outfile:
+        json.dump(drivers_data, outfile)
 
 @app.route('/')
 def home():
@@ -35,17 +32,19 @@ def home():
 
 @app.route('/api/standings.json')
 def standings():
-    drivers = increment_points(get_drivers())
-    return json.dumps(drivers)
+    drivers =  get_data_from_file('drivers.json')
+    drivers_updated = increment_points(drivers)
+    update_drivers(drivers_updated)
+    return json.dumps(drivers_updated)
 
 
 @app.route('/api/team/<int:team_id>.json')
 def team_details(team_id):
-    teams_by_id = get_teams_by_id()
-    try:
-        return json.dumps(teams_by_id[str(team_id)])
-    except (KeyError):
-        return jsonify(error=404, text="Not found"), 404
+    teams = get_data_from_file('teams.json')
+    for team in teams:
+        if team['id'] == team_id:
+            return json.dumps(team)
+    return  jsonify(error=404, text="Not found"), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
