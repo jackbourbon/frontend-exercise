8c99ab52262f04db796ae2f3c1e633c903845e95
Alberto Vila <alberto.vilatena@gmail.com>
2016-01-01 20:33:20 +0200

    Using angular directives to deal with images. Some comments in the code

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,11 @@
 var grandprixApp = angular.module('grandprixApp', ['ngRoute']);
 
+//Every second, wherever we are in the app, we get the standings
 grandprixApp.run(['Standings','$interval', function(Standings, $interval) {
   $interval(Standings.getDrivers.bind(Standings), 1000);
 }]);
 
-//Router
+// ROUTER
 grandprixApp.config(['$routeProvider', function($routeProvider){
     $routeProvider.
       when('/', {
@@ -20,26 +21,29 @@ grandprixApp.config(['$routeProvider', function($routeProvider){
       });
 }]);
 
-//Services
+// SERVICES
 angular.module('grandprixApp')
     .service('api', ['$http', function ($http) {
         var urlBase = 'api';
 
+        // API call to get a promise for standings
         this.getStandings = function(){
             return $http.get(urlBase + '/standings.json');
         };
 
+        // API call to get a promise for a team object
         this.getTeamInfo = function(teamId){
             return $http.get(urlBase + '/team/' + teamId + '.json');
         };
 }]);
 
-//Factories
+// FACTORIES
 grandprixApp.factory('Standings', ['api', '$filter', function(api, $filter) {
     var Standings = {
         drivers: []
     };
 
+    // Retrieves the drivers from the API and returns them sorted by their number of points
     Standings.getDrivers = function() {
         var updatedStandings = this;
 
@@ -51,6 +55,7 @@ grandprixApp.factory('Standings', ['api', '$filter', function(api, $filter) {
     return Standings;
 }]);
 
+// CONTROLLERS AND DIRECTIVES
 grandprixApp.controller('StandingsCtrl', ['$scope', 'Standings', function($scope, Standings){
     $scope.Standings = Standings;
     $scope.teams = {}
@@ -58,6 +63,7 @@ grandprixApp.controller('StandingsCtrl', ['$scope', 'Standings', function($scope
 .directive('myTeamFinder', ['api', function(api) {
     return {
         template: '<a href="#/teams/{{driver.team}}">{{teams[driver.team]}}</a>',
+        // This directive is used to retrieve and show the name of the driver's team in the standings
         link: function(scope){
             api.getTeamInfo(scope.driver.team).success(function(data){
                 scope.teams[scope.driver.team] = data.team;
